564ed6d3822d099350b9fd871f2cce52863569cb
coldwind444 <minhtan14042004@gmail.com>
2025-05-07 14:01:41 +0700

    modify embedded id

--- src/main/java/com/be/java/foxbase/db/key/UserBookId.java
+++ src/main/java/com/be/java/foxbase/db/key/UserBookId.java
@@ -4,13 +4,15 @@ import jakarta.persistence.Embeddable;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serializable;
+
 @Embeddable
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @EqualsAndHashCode
-public class UserBookId {
+public class UserBookId implements Serializable {
     String username;
     Long bookId;
 }
